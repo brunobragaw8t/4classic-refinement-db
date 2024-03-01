dd2c268468ea602340e45d76c5089246b1b4668d
Bruno Braga <brunobragaw8t@gmail.com>
2024-03-01 19:44:39 +0000

    chore: setup Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon'],
+  modules: ['nuxt-icon', '@nuxtjs/tailwindcss'],
   devtools: { enabled: false }
 })
