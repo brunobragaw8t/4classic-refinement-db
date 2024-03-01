8a7a621e6163202c5149ab9d3467bf1ad5fe62dc
Bruno Braga <brunobragaw8t@gmail.com>
2024-03-01 22:28:36 +0000

    chore: add more data

--- db/db.ts
+++ db/db.ts
@@ -55,6 +55,96 @@ export const db: DB = {
               color: 'red'
             }
           ]
+        },
+        {
+          name: 'Crebata\'s Hard Leather Gloves',
+          stats: [
+            {
+              name: 'Evade',
+              value: 3,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 3,
+              color: 'red'
+            }
+          ],
+          result: [
+            {
+              name: 'Evade',
+              value: 9,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 9,
+              color: 'red'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Cloth Armour Top',
+      imgs: [
+        'Clothing of Arkana',
+        'Pauldron\'s Embrace'
+      ],
+      itemsToRefineWith: [
+        {
+          name: 'Aunloke\'s Light Armour Gloves',
+          stats: [
+            {
+              name: 'Evade',
+              value: 3,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 3,
+              color: 'red'
+            }
+          ],
+          result: [
+            {
+              name: 'Evade',
+              value: 6,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 12,
+              color: 'red'
+            }
+          ]
+        },
+        {
+          name: 'Crebata\'s Hard Leather Gloves',
+          stats: [
+            {
+              name: 'Evade',
+              value: 3,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 3,
+              color: 'red'
+            }
+          ],
+          result: [
+            {
+              name: 'Evade',
+              value: 7,
+              color: 'blue'
+            },
+            {
+              name: 'Resistance',
+              value: 11,
+              color: 'blue'
+            }
+          ]
         }
       ]
     }
